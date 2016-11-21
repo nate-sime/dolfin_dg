ecbeae91ace24976421a0b9e55a0abb604c2ad92
Nate Sime <njcs4@cam.ac.uk>
2016-11-21 19:29:24 +0000

    add ringleb to defaults

--- dolfin_dg/__init__.py
+++ dolfin_dg/__init__.py
@@ -4,3 +4,4 @@ __author__ = 'njcs4'
 from dg_form import DGFemViscousTerm, hyper_tensor_product, hyper_tensor_T_product
 from fluxes import force_zero_function_derivative
 from tensors import compressible_ns_G, compressible_ns_entopy_G
+import ringleb
