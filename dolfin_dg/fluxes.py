629e1dac53ac52846b9d93be1177e3d4f8ab359a
Nate Sime <njcs4@cam.ac.uk>
2017-03-01 18:50:59 +0000

    llf

--- dolfin_dg/fluxes.py
+++ dolfin_dg/fluxes.py
@@ -1,4 +1,5 @@
 from ufl.restriction import Restricted
+from ufl import dot, Constant
 from dolfin import Function
 
 __author__ = 'njcs4'
@@ -11,3 +12,8 @@ def force_zero_function_derivative(avec):
     if isinstance(avec, Function):
         return avec.copy()
     return avec
+
+def lax_friedrichs_flux(F_c, alpha):
+    def H(U_p, U_m, n_p):
+        return 0.5*(dot(F_c(U_p), n_p) + dot(F_c(U_m), n_p) + alpha*(U_p - U_m))
+    return H
