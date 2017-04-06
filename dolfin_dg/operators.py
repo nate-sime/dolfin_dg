f9ae294ae6cbf057320ecdb5f79fa0785b641388
Nate Sime <njcs4@cam.ac.uk>
2017-04-06 19:53:10 +0100

    oeprators update for curl

--- dolfin_dg/operators.py
+++ dolfin_dg/operators.py
@@ -1,4 +1,5 @@
-from dolfin_dg.dg_form import DGFemViscousTerm, homogeneity_tensor, tangent_jump, tensor_jump, ufl_adhere_transpose
+from dolfin_dg.dg_form import DGFemViscousTerm, homogeneity_tensor, tangent_jump, tensor_jump, ufl_adhere_transpose, \
+    DGFemCurlTerm
 import ufl
 from ufl import CellVolume, FacetArea, grad, inner, \
     div, jump, avg, curl, cross, Max, dot, as_vector, as_matrix, sqrt, tr, Identity, variable, diff, exp
@@ -89,37 +90,11 @@ class PoissonOperator(EllipticOperator):
         EllipticOperator.__init__(self, mesh, fspace, bcs, F_v)
 
 
-class StokesOperator(EllipticOperator):
-
-    def __init__(self, mesh, V, bcs, mu=1):
-        def F_v(u, grad_u):
-            return mu*grad_u
-
-        EllipticOperator.__init__(self, mesh, V, bcs, F_v)
-
-    def generate_fem_formulation(self, u, v, dx=None, dS=None):
-
-        u, p = split(u)
-        v, q = split(v)
-        n = FacetNormal(self.mesh)
-
-        def B(v, q):
-            return -q*div(v)*dx + avg(q)*jump(v, n)*dS \
-                   + sum(q*inner(v, n)*bc.get_boundary() for bc in self.dirichlet_bcs)
-
-        residual = EllipticOperator.generate_fem_formulation(self, u, v)
-        residual += B(v, p) - B(u, q)
-        residual -= sum(-q*inner(bc.get_function(), n)*bc.get_boundary() for bc in self.dirichlet_bcs)
-
-        return residual
-
-
 class MaxwellOperator(FemFormulation):
 
-    def __init__(self, mesh, V, bcs, mu_r=1, eps_r=1, C_IP=10.0):
-        FemFormulation.__init__(self, mesh, V, bcs)
-        self.mu_r = mu_r
-        self.eps_r = eps_r
+    def __init__(self, mesh, fspace, bcs, F_m, C_IP=10.0):
+        FemFormulation.__init__(self, mesh, fspace, bcs)
+        self.F_m = F_m
         self.C_IP = C_IP
 
     def generate_fem_formulation(self, u, v, dx=None, dS=None):
@@ -128,50 +103,21 @@ class MaxwellOperator(FemFormulation):
         if dS is None:
             dS = Measure('dS', domain=self.mesh)
 
-        E, p = split(u)
-        F, q = split(v)
-
         h = CellVolume(self.mesh)/FacetArea(self.mesh)
         n = FacetNormal(self.mesh)
-
-        mu_r, eps_r = self.mu_r, self.eps_r
         sigma = self.C_IP*Constant(max(self.fspace.ufl_element().degree()**2, 1))/h
-        a_disp = self.C_IP/mu_r*sigma
-        c = self.C_IP*eps_r*sigma
+        curl_u = variable(curl(u))
+        G = diff(self.F_m(u, curl_u), curl_u)
+        ct = DGFemCurlTerm(self.F_m, u, v, sigma, G, n)
 
-        def A(u, v):
-            val = 1.0/mu_r*inner(curl(u), curl(v)) * dx \
-                  - inner(tangent_jump(u, n), avg(1.0/mu_r*curl(v))) * dS \
-                  - inner(tangent_jump(v, n), avg(1.0/mu_r*curl(u))) * dS \
-                  + a_disp('+') * inner(tangent_jump(u, n), tangent_jump(v, n)) * dS
-            for bc in self.dirichlet_bcs:
-                dSD = bc.get_boundary()
-                val += - inner(cross(n, u), 1.0/mu_r*curl(v)) * dSD \
-                       - inner(cross(n, v), 1.0/mu_r*curl(u)) * dSD \
-                       + inner(cross(n, u), cross(n, a_disp * v)) * dSD
-            return val
+        residual = inner(self.F_m(u, curl(u)), curl(v))*dx
+        residual += ct.interior_residual(dS)
 
-        def B(v, p):
-            val = -eps_r*inner(v, grad(p))*dx + inner(avg(eps_r*v), jump(p, n))*dS
-            for bc in self.dirichlet_bcs:
-                val += inner(eps_r*v, p*n)*bc.get_boundary()
-            return val
+        for dbc in self.dirichlet_bcs:
+            residual += ct.exterior_residual(dbc.get_function(), dbc.get_boundary())
 
-        def C(p, q):
-            val = c('+')*inner(jump(p), jump(q))*dS
-            for bc in self.dirichlet_bcs:
-                val += c*p*q*bc.get_boundary()
-            return val
-
-        def L(v):
-            val = 0
-            for g, dS_p in [(bc.get_function(), bc.get_boundary()) for bc in self.dirichlet_bcs]:
-                val += - inner(cross(n, g), 1.0/mu_r*curl(v))*dS_p + a_disp*inner(cross(n, g), cross(n, v))*dS_p
-            for gN, dS_pN in [(bc.get_function(), bc.get_boundary()) for bc in self.neumann_bcs]:
-                val -= inner(gN, v)*dS_pN
-            return val
-
-        residual = A(E, F) + B(F, p) - L(F) + B(E, q) - C(p, q)
+        for dbc in self.neumann_bcs:
+            residual += ct.neumann_residual(dbc.get_function(), dbc.get_boundary())
 
         return residual
 
