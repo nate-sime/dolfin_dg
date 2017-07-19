bb7409b496acf20c650575d1930af77db6610535
Nate Sime <pmxns2@Nates-MBP.home>
2017-07-19 21:44:33 +0100

    linear poisson demo using hypre and CG

---
+++ demo/dg_linear_poisson_hypre_amg.py
@@ -0,0 +1,56 @@
+from dolfin import *
+from dolfin_dg import *
+import numpy as np
+
+__author__ = 'njcs4'
+
+parameters['form_compiler']["cpp_optimize"] = True
+parameters['form_compiler']["optimize"] = True
+parameters['form_compiler']['representation'] = 'uflacs'
+parameters["ghost_mode"] = "shared_facet"
+
+run_count = 0
+ele_ns = [4, 8, 16, 32, 64]
+errorl2 = np.zeros(len(ele_ns))
+errorh1 = np.zeros(len(ele_ns))
+hsizes = np.zeros(len(ele_ns))
+p = 2
+
+for ele_n in ele_ns:
+    mesh = UnitSquareMesh(ele_n, ele_n)
+
+    V = FunctionSpace(mesh, 'DG', p)
+    u, v = Function(V), TestFunction(V)
+
+    gD = Expression('sin(pi*x[0])*sin(pi*x[1])',
+                   element=V.ufl_element())
+    f = Expression('2*pi*pi*sin(pi*x[0])*sin(pi*x[1])',
+                   element=V.ufl_element())
+
+    pe = PoissonOperator(mesh, V, DGDirichletBC(ds, gD))
+    F = pe.generate_fem_formulation(u, v) - f*v*dx
+    J = derivative(F, u)
+
+    A, b = PETScMatrix(), PETScVector()
+    assemble_system(J, -F, A_tensor=A, b_tensor=b)
+
+    solver = PETScKrylovSolver()
+    solver.set_operator(A)
+
+    PETScOptions.set("ksp_type", "cg")
+    PETScOptions.set("pc_type", "hypre")
+    PETScOptions.set("ksp_monitor_true_residual")
+    PETScOptions.set("ksp_rtol", 1e-12)
+    solver.set_from_options()
+
+    solver.solve(u.vector(), b)
+
+    errorl2[run_count] = errornorm(gD, u, norm_type='l2', degree_rise=3)
+    errorh1[run_count] = errornorm(gD, u, norm_type='h1', degree_rise=3)
+    hsizes[run_count] = mesh.hmax()
+
+    run_count += 1
+
+if dolfin.MPI.rank(mesh.mpi_comm()) == 0:
+    print(np.log(errorl2[0:-1]/errorl2[1:])/np.log(hsizes[0:-1]/hsizes[1:]))
+    print(np.log(errorh1[0:-1]/errorh1[1:])/np.log(hsizes[0:-1]/hsizes[1:]))
