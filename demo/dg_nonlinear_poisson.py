1bb0d23f20b6c3501b10ca361f3bc67032f83207
Nate Sime <njcs4@cam.ac.uk>
2017-07-27 19:38:58 +0100

    nonlinear poisson demo

---
+++ demo/dg_nonlinear_poisson.py
@@ -0,0 +1,31 @@
+from dolfin import *
+from dolfin_dg import *
+import numpy as np
+
+__author__ = 'njcs4'
+
+parameters["ghost_mode"] = "shared_facet"
+
+mesh = UnitSquareMesh(32, 32)
+
+V = FunctionSpace(mesh, 'DG', 1)
+u, v = Function(V), TestFunction(V)
+
+f = Expression("x[0]*sin(x[1])", degree=1)
+
+facets = FacetFunction("size_t", mesh, 0)
+AutoSubDomain(lambda x, on_boundary: near(x[0], 1.0) and on_boundary).mark(facets, 1)
+
+ds = Measure("ds", subdomain_data=facets)
+
+g = Constant(1.0)
+bc = DGDirichletBC(ds(1), g)
+
+pe = PoissonOperator(mesh, V, [bc], kappa=(1 + u**2))
+F = pe.generate_fem_formulation(u, v) - f*v*dx
+
+solve(F == 0, u, solver_parameters={"newton_solver": {"linear_solver": "lu"}})
+
+plot(u, title="Solution")
+plot(grad(u), title="Solution gradient")
+interactive()
