4e4531fe91dd43de31567c44dfd4fa72a27cb657
Nate Sime <njcs4@cam.ac.uk>
2017-09-20 20:06:18 +0100

    add HLLE example

--- dolfin_dg/fluxes.py
+++ dolfin_dg/fluxes.py
@@ -1,8 +1,82 @@
 import ufl
-from ufl import dot, Max
+from ufl import dot, Max, Min
 
 __author__ = 'njcs4'
 
+
+def max_abs_of_sequence(a):
+    """
+    Utility function to generate the Max Abs of a sequence of N elements
+    e.g.: Max(|a1|, |a2|, |a3|, ..., |aN|).
+
+    This is required because (currently) ufl only allows two values in
+    the constuctor of Max().
+
+    :param a: sequence of ufl elements
+    :return:
+    """
+    if isinstance(a, ufl.core.expr.Expr):
+        return abs(a)
+
+    assert isinstance(a, (list, tuple))
+
+    a = list(map(abs, a))
+    return max_of_sequence(a)
+
+
+def max_of_sequence(a):
+    """
+    Utility function to generate the Max of a sequence of N elements
+    e.g.: Max(a1, a2, a3, ..., aN).
+
+    This is required because (currently) ufl only allows two values in
+    the constuctor of Max().
+
+    :param a: sequence of ufl elements
+    :return:
+    """
+    return map_ufl_operator_to_sequence(a, Max)
+
+
+def min_of_sequence(a):
+    """
+    Utility function to generate the Max of a sequence of N elements
+    e.g.: Max(a1, a2, a3, ..., aN).
+
+    This is required because (currently) ufl only allows two values in
+    the constuctor of Max().
+
+    :param a: sequence of ufl elements
+    :return:
+    """
+    return map_ufl_operator_to_sequence(a, Min)
+
+
+def map_ufl_operator_to_sequence(a, op):
+    """
+    Utility function to generate the op of a sequence of N elements
+    e.g.: op(a1, a2, a3, ..., aN).
+
+    This is required because (currently and commonly) ufl only allows
+    two values in the constuctors of MathOperator.
+
+    Warning: If the sequence has length 1, op(a) is assumed == a
+
+    This is intended to be used with Min and Max
+
+    :param a: sequence of ufl elements
+    :param op: a ufl MathOperator
+    :return:
+    """
+    if isinstance(a, ufl.core.expr.Expr):
+        return a
+
+    assert isinstance(a, (list, tuple))
+
+    alpha = op(a[0], a[1])
+    for j in range(2, len(a)):
+        alpha = op(alpha, a[j])
+    return alpha
 
 class ConvectiveFlux:
 
@@ -21,13 +95,13 @@ class ConvectiveFlux:
 
 class LocalLaxFriedrichs:
 
-    def __init__(self, alpha_func):
-        self.alpha_func = alpha_func
+    def __init__(self, flux_jacobian_eigenvalues):
+        self.flux_jacobian_eigenvalues = flux_jacobian_eigenvalues
         self.alpha = None
 
     def setup(self, F_c, u_p, u_m, n):
-        eigen_vals_max_p = self.max_abs_eigenvalues(self.alpha_func(u_p, n))
-        eigen_vals_max_m = self.max_abs_eigenvalues(self.alpha_func(u_m, n))
+        eigen_vals_max_p = max_abs_of_sequence(self.flux_jacobian_eigenvalues(u_p, n))
+        eigen_vals_max_m = max_abs_of_sequence(self.flux_jacobian_eigenvalues(u_m, n))
         self.alpha = Max(eigen_vals_max_p, eigen_vals_max_m)
 
     def interior(self, F_c, u_p, u_m, n):
@@ -36,14 +110,24 @@ class LocalLaxFriedrichs:
     def exterior(self, F_c, u_p, u_m, n):
         return 0.5*(dot(F_c(u_p), n) + dot(F_c(u_m), n) + self.alpha*(u_p - u_m))
 
-    def max_abs_eigenvalues(self, lambdas):
-        if isinstance(lambdas, ufl.core.expr.Expr):
-            return abs(lambdas)
 
-        assert isinstance(lambdas, (list, tuple))
+class HLLE:
 
-        lambdas = list(map(abs, lambdas))
-        alpha = Max(lambdas[0], lambdas[1])
-        for j in range(2, len(lambdas)):
-            alpha = Max(alpha, lambdas[j])
-        return alpha
+    def __init__(self, flux_jacobian_eigenvalues):
+        self.flux_jacobian_eigenvalues = flux_jacobian_eigenvalues
+
+    def setup(self, F_c, u_p, u_m, n):
+        eigen_vals_max_p = max_of_sequence(self.flux_jacobian_eigenvalues(u_p, n))
+        eigen_vals_min_m = min_of_sequence(self.flux_jacobian_eigenvalues(u_m, n))
+        self.lam_p = Max(eigen_vals_max_p, 0)
+        self.lam_m = Min(eigen_vals_min_m, 0)
+
+    def interior(self, F_c, u_p, u_m, n):
+        lam_p, lam_m = self.lam_p, self.lam_m
+        guard = ufl.conditional(abs(lam_p - lam_m) < 1e-7, 0, 1/(lam_p - lam_m))
+        return guard*(lam_p*dot(F_c(u_p), n) - lam_m*dot(F_c(u_m), n) - lam_p*lam_m*(u_p - u_m))
+
+    def exterior(self, F_c, u_p, u_m, n):
+        lam_p, lam_m = self.lam_p, self.lam_m
+        guard = ufl.conditional(abs(lam_p - lam_m) < 1e-7, 0, 1/(lam_p - lam_m))
+        return guard*(lam_p*dot(F_c(u_p), n) - lam_m*dot(F_c(u_m), n) - lam_p*lam_m*(u_p - u_m))
