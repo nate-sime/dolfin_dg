d02b60b9dd1ec7a6b8dda5c45ab44438c08b064a
Nate Sime <njcs4@cam.ac.uk>
2017-09-26 18:30:20 +0100

    update hypre demo to go 3d

--- demo/dg_linear_poisson_hypre_amg.py
+++ demo/dg_linear_poisson_hypre_amg.py
@@ -2,29 +2,34 @@ from dolfin import *
 from dolfin_dg import *
 import numpy as np
 
+
 __author__ = 'njcs4'
 
+
+parameters['std_out_all_processes'] = False
 parameters['form_compiler']["cpp_optimize"] = True
 parameters['form_compiler']["optimize"] = True
 parameters['form_compiler']['representation'] = 'uflacs'
 parameters["ghost_mode"] = "shared_facet"
 
+
 run_count = 0
-ele_ns = [4, 8, 16, 32, 64]
+ele_ns = [4, 8, 16, 32]
 errorl2 = np.zeros(len(ele_ns))
 errorh1 = np.zeros(len(ele_ns))
 hsizes = np.zeros(len(ele_ns))
-p = 2
+p = 1
+
 
 for ele_n in ele_ns:
-    mesh = UnitSquareMesh(ele_n, ele_n)
+    mesh = UnitCubeMesh(ele_n, ele_n, ele_n)
 
     V = FunctionSpace(mesh, 'DG', p)
     u, v = Function(V), TestFunction(V)
 
-    gD = Expression('sin(pi*x[0])*sin(pi*x[1])',
+    gD = Expression('sin(pi*x[0])*sin(pi*x[1])*sin(pi*x[2])',
                    element=V.ufl_element())
-    f = Expression('2*pi*pi*sin(pi*x[0])*sin(pi*x[1])',
+    f = Expression('3*pi*pi*sin(pi*x[0])*sin(pi*x[1])*sin(pi*x[2])',
                    element=V.ufl_element())
 
     pe = PoissonOperator(mesh, V, DGDirichletBC(ds, gD))
@@ -40,9 +45,10 @@ for ele_n in ele_ns:
     PETScOptions.set("ksp_type", "cg")
     PETScOptions.set("pc_type", "hypre")
     PETScOptions.set("ksp_monitor_true_residual")
-    PETScOptions.set("ksp_rtol", 1e-12)
+    PETScOptions.set("ksp_rtol", 1e-6)
     solver.set_from_options()
 
+    info("Solving problem with %d degrees of freedom" % V.dim())
     solver.solve(u.vector(), b)
 
     errorl2[run_count] = errornorm(gD, u, norm_type='l2', degree_rise=3)
@@ -52,5 +58,5 @@ for ele_n in ele_ns:
     run_count += 1
 
 if dolfin.MPI.rank(mesh.mpi_comm()) == 0:
-    print(np.log(errorl2[0:-1]/errorl2[1:])/np.log(hsizes[0:-1]/hsizes[1:]))
-    print(np.log(errorh1[0:-1]/errorh1[1:])/np.log(hsizes[0:-1]/hsizes[1:]))
+    print("L2 convergence rates: " + str(np.log(errorl2[0:-1]/errorl2[1:])/np.log(hsizes[0:-1]/hsizes[1:])))
+    print("H1 convergence rates: " + str(np.log(errorh1[0:-1]/errorh1[1:])/np.log(hsizes[0:-1]/hsizes[1:])))
