ff6b57af59daf25dbeb4721c27d710134462352c
nate-sime <nsime@carnegiescience.edu>
2020-07-30 13:47:48 -0400

    call fluxes what they are

--- dolfin_dg/hdg_form.py
+++ dolfin_dg/hdg_form.py
@@ -77,37 +77,38 @@ class HDGClassicalFirstOrder:
         def facet_int(integrand):
             return facet_integral(integrand, dInt, dExt)
 
-        S = ufl.Min(self.H_flux.flux_jacobian_eigenvalues(ubar, n), 0)
-
-        F_c_flux = ufl.dot(self.F_c(ubar), n) + S * (u - ubar)
-
-        residual0 = facet_int(ufl.inner(F_c_flux, v))
-
-        residual1 = -facet_int(ufl.inner(F_c_flux, vbar))
-
-        residual = residual0 + residual1
-
-        # # Garth
-        # S_p = ufl.Max(self.H_flux.flux_jacobian_eigenvalues(u, n), 0)
-        # S_m = ufl.Min(self.H_flux.flux_jacobian_eigenvalues(u, n), 0)
+        # `LF' flux
+        # S = ufl.Min(self.H_flux.flux_jacobian_eigenvalues(ubar, n), 0)
         #
-        # # interior
-        # F_c_flux = S_p * u + S_m * ubar
-        # residual0 = -facet_int(ufl.inner(F_c_flux, vbar - v))
+        # F_c_flux = ufl.dot(self.F_c(ubar), n) + S * (u - ubar)
         #
-        # # exterior
-        # residual1 = 0
-        # for bc in self.dg_bcs:
-        #     if not isinstance(bc, DGDirichletBC):
-        #         continue
-        #     ds_bc = bc.get_boundary()
-        #     gD = bc.get_function()
+        # residual0 = facet_int(ufl.inner(F_c_flux, v))
         #
-        #     S_m = ufl.Min(self.H_flux.flux_jacobian_eigenvalues(gD, n), 0)
-        #     F_c_flux = S_p * ubar + S_m * gD
-        #     residual1 += ufl.inner(F_c_flux, vbar)*ds_bc
+        # residual1 = -facet_int(ufl.inner(F_c_flux, vbar))
         #
         # residual = residual0 + residual1
+
+        # `Vijayasundaram' flux
+        S_p = ufl.Max(self.H_flux.flux_jacobian_eigenvalues(u, n), 0)
+        S_m = ufl.Min(self.H_flux.flux_jacobian_eigenvalues(u, n), 0)
+
+        # interior
+        F_c_flux = S_p * u + S_m * ubar
+        residual0 = -facet_int(ufl.inner(F_c_flux, vbar - v))
+
+        # exterior
+        residual1 = 0
+        for bc in self.dg_bcs:
+            if not isinstance(bc, DGDirichletBC):
+                continue
+            ds_bc = bc.get_boundary()
+            gD = bc.get_function()
+
+            S_m = ufl.Min(self.H_flux.flux_jacobian_eigenvalues(gD, n), 0)
+            F_c_flux = S_p * ubar + S_m * gD
+            residual1 += ufl.inner(F_c_flux, vbar)*ds_bc
+
+        residual = residual0 + residual1
 
         return residual
 
