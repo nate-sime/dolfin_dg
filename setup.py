4cadcf755a7805ea3256a9a721134824e83c22bd
nate-sime <nsime@carnegiescience.edu>
2020-08-06 16:51:14 -0400

    flake8 setup file and exclude paper examples and firedrake from flake8

--- setup.py
+++ setup.py
@@ -11,10 +11,11 @@ if sys.version_info < (3, 6):
     print("Python 3.6 or higher required.")
     sys.exit(1)
 
-version = '2019.6.11'
+version = '2020.1.0'
 
 setup(name="dolfin_dg",
-      description="Utility module for automatic generation of DG FE formulations.",
+      description="Utility module for automatic generation of DG FE "
+                  "formulations.",
       version=version,
       author="Nate Sime",
       author_email="njcs4@cam.ac.uk",
