d43e6e3dafa16f45fbf63654b3a82c9c4275d21e
nate-sime <nsime@carnegiescience.edu>
2021-09-08 17:56:01 -0400

    facet average until trace spaces become available

--- dolfin_dg/dolfinx/__init__.py
+++ dolfin_dg/dolfinx/__init__.py
@@ -1,1 +1,2 @@
 from .nls import MatrixType
+from .util import cell_volume_dg0, facet_area_avg_dg0
---
+++ dolfin_dg/dolfinx/util.py
@@ -0,0 +1,44 @@
+from petsc4py import PETSc
+import ufl
+import dolfinx
+
+
+def _form_with_estimated_quad_degree(mesh, form, quadrature_degree):
+    if quadrature_degree is None:
+        quadrature_degree = \
+            mesh.ufl_domain().ufl_coordinate_element().degree() + 1
+
+    dolfinx_form = dolfinx.fem.Form(
+        form, form_compiler_parameters={
+            "quadrature_degree": quadrature_degree})
+
+    return dolfinx_form
+
+
+def facet_area_avg_dg0(mesh, quadrature_degree=None):
+    DG0 = dolfinx.FunctionSpace(mesh, ("DG", 0))
+    facet_area_avg = dolfinx.Function(DG0)
+    v_dg = ufl.TestFunction(DG0)
+    num_facets = dolfinx.cpp.mesh.cell_num_entities(
+        mesh.topology.cell_type, mesh.topology.dim - 1)
+    avg_facet_area = ((v_dg("+") + v_dg("-"))/num_facets)*ufl.dS \
+                     + (v_dg/num_facets)*ufl.ds
+    dolfinx_form = _form_with_estimated_quad_degree(
+        mesh, avg_facet_area, quadrature_degree)
+    dolfinx.fem.assemble_vector(facet_area_avg.vector, dolfinx_form)
+    facet_area_avg.vector.ghostUpdate(
+        addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
+    return facet_area_avg
+
+
+def cell_volume_dg0(mesh, quadrature_degree=None):
+    DG0 = dolfinx.FunctionSpace(mesh, ("DG", 0))
+    cell_volume = dolfinx.Function(DG0)
+    v_dg = ufl.TestFunction(DG0)
+    cell_volume_form = v_dg*ufl.dx
+    dolfinx_form = _form_with_estimated_quad_degree(
+        mesh, cell_volume_form, quadrature_degree)
+    dolfinx.fem.assemble_vector(cell_volume.vector, dolfinx_form)
+    cell_volume.vector.ghostUpdate(
+        addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
+    return cell_volume
